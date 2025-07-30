a60a4de87297eb99fc82bb4ebd8c24840d368ef8
liorboyango <master010590@gmail.com>
2025-07-30 15:19:46 +0300

    Initial Commit

---
+++ android/app/src/main/kotlin/com/coolers/cool_app_fe/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.coolers.cool_app_fe
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,83 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Collest App Ever',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Collest Main Screen Ever!'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String _status = 'Fetching...';
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchServerStatus();
+  }
+
+  Future<void> _fetchServerStatus() async {
+    try {
+      final response = await http.get(
+        Uri.parse('http://localhost:3000/api/status'),
+      );
+
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        setState(() {
+          _status = 'Status: ${data['status']}, Time: ${data['timestamp']}';
+        });
+      } else {
+        setState(() {
+          _status = 'Server error: ${response.statusCode}';
+        });
+      }
+    } catch (e) {
+      setState(() {
+        _status = 'Error: $e';
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: theme.colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Text(
+          _status,
+          style: theme.textTheme.headlineSmall,
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+  }
+}
