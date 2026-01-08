5f6658e2f5be07aaec1358b2a7bb705c1b713c86
Liorbo <master010590@gmail.com>
2026-01-08 08:28:07 +0200

    Make entire theme toggle tile clickable by adding onTap to ListTile and removing tap handling from ThemeToggle widget

--- lib/theme_toggle.dart
+++ lib/theme_toggle.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class ThemeToggle extends StatefulWidget {
+class ThemeToggle extends StatelessWidget {
   final bool isDark;
   final Function(bool) onToggle;
 
@@ -11,84 +11,58 @@ class ThemeToggle extends StatefulWidget {
   });
 
   @override
-  State<ThemeToggle> createState() => _ThemeToggleState();
-}
-
-class _ThemeToggleState extends State<ThemeToggle> {
-  late bool _isDark;
-
-  @override
-  void initState() {
-    super.initState();
-    _isDark = widget.isDark;
-  }
-
-  @override
-  void didUpdateWidget(covariant ThemeToggle oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    _isDark = widget.isDark;
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        _isDark = !_isDark;
-        widget.onToggle(_isDark);
-        setState(() {});
-      },
-      child: AnimatedContainer(
-        duration: const Duration(milliseconds: 200),
-        width: 70,
-        height: 34,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20),
-          color: _isDark ? Colors.grey[800] : Colors.yellow[100],
-        ),
-        child: Stack(
-          children: [
-            AnimatedPositioned(
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 200),
+      width: 70,
+      height: 34,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: isDark ? Colors.grey[800] : Colors.yellow[100],
+      ),
+      child: Stack(
+        children: [
+          AnimatedPositioned(
+            duration: const Duration(milliseconds: 200),
+            curve: Curves.easeInOut,
+            top: 4,
+            left: isDark ? 36 : 4,
+            child: Container(
+              width: 26,
+              height: 26,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          Positioned(
+            left: 5,
+            top: 5,
+            child: AnimatedOpacity(
               duration: const Duration(milliseconds: 200),
-              curve: Curves.easeInOut,
-              top: 4,
-              left: _isDark ? 36 : 4,
-              child: Container(
-                width: 26,
-                height: 26,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Colors.white,
-                ),
+              opacity: isDark ? 0.5 : 1.0,
+              child: const Icon(
+                Icons.wb_sunny,
+                color: Colors.orange,
+                size: 24,
               ),
             ),
-            Positioned(
-              left: 5,
-              top: 5,
-              child: AnimatedOpacity(
-                duration: const Duration(milliseconds: 200),
-                opacity: _isDark ? 0.5 : 1.0,
-                child: const Icon(
-                  Icons.wb_sunny,
-                  color: Colors.orange,
-                  size: 24,
-                ),
+          ),
+          Positioned(
+            right: 5,
+            top: 5,
+            child: AnimatedOpacity(
+              duration: const Duration(milliseconds: 200),
+              opacity: isDark ? 1.0 : 0.5,
+              child: const Icon(
+                Icons.nightlight_round,
+                color: Colors.blueGrey,
+                size: 24,
               ),
             ),
-            Positioned(
-              right: 5,
-              top: 5,
-              child: AnimatedOpacity(
-                duration: const Duration(milliseconds: 200),
-                opacity: _isDark ? 1.0 : 0.5,
-                child: const Icon(
-                  Icons.nightlight_round,
-                  color: Colors.blueGrey,
-                  size: 24,
-                ),
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
