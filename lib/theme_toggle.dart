813876585e9ac04f30b110a954b5dd709af59f31
Liorbo <master010590@gmail.com>
2026-01-09 14:01:54 +0200

    Fix ThemeToggle widget to be tappable, use AppColors for circle, and add semantic labels for accessibility

--- lib/theme_toggle.dart
+++ lib/theme_toggle.dart
@@ -14,57 +14,67 @@ class ThemeToggle extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedContainer(
-      duration: const Duration(milliseconds: 200),
-      width: 70,
-      height: 34,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        color: isDark ? AppColors.darkSurface : AppColors.lightSurfaceVariant,
-      ),
-      child: Stack(
-        children: [
-          AnimatedPositioned(
-            duration: const Duration(milliseconds: 200),
-            curve: Curves.easeInOut,
-            top: 4,
-            left: isDark ? 36 : 4,
-            child: Container(
-              width: 26,
-              height: 26,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: Colors.white,
-              ),
-            ),
-          ),
-          Positioned(
-            left: 5,
-            top: 5,
-            child: AnimatedOpacity(
+    return InkWell(
+      onTap: () => onToggle(!isDark),
+      borderRadius: BorderRadius.circular(20),
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 200),
+        width: 70,
+        height: 34,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: isDark ? AppColors.darkSurface : AppColors.lightSurfaceVariant,
+        ),
+        child: Stack(
+          children: [
+            AnimatedPositioned(
               duration: const Duration(milliseconds: 200),
-              opacity: isDark ? 0.5 : 1.0,
-              child: Icon(
-                Icons.wb_sunny,
-                color: AppColors.lightWarning,
-                size: 24,
+              curve: Curves.easeInOut,
+              top: 4,
+              left: isDark ? 36 : 4,
+              child: Container(
+                width: 26,
+                height: 26,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: isDark ? AppColors.darkOnPrimary : AppColors.lightOnPrimary,
+                ),
               ),
             ),
-          ),
-          Positioned(
-            right: 5,
-            top: 5,
-            child: AnimatedOpacity(
-              duration: const Duration(milliseconds: 200),
-              opacity: isDark ? 1.0 : 0.5,
-              child: Icon(
-                Icons.nightlight_round,
-                color: AppColors.darkPrimary,
-                size: 24,
+            Positioned(
+              left: 5,
+              top: 5,
+              child: AnimatedOpacity(
+                duration: const Duration(milliseconds: 200),
+                opacity: isDark ? 0.5 : 1.0,
+                child: Semantics(
+                  label: 'Switch to light theme',
+                  child: Icon(
+                    Icons.wb_sunny,
+                    color: AppColors.lightWarning,
+                    size: 24,
+                  ),
+                ),
               ),
             ),
-          ),
-        ],
+            Positioned(
+              right: 5,
+              top: 5,
+              child: AnimatedOpacity(
+                duration: const Duration(milliseconds: 200),
+                opacity: isDark ? 1.0 : 0.5,
+                child: Semantics(
+                  label: 'Switch to dark theme',
+                  child: Icon(
+                    Icons.nightlight_round,
+                    color: AppColors.darkPrimary,
+                    size: 24,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
