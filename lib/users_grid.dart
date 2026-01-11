2c48db6dad8af181c26ca4fe96aa4abf5e29774d
Liorbo <master010590@gmail.com>
2026-01-11 17:51:10 +0200

    Add name fallback in UsersGrid itemBuilder

--- lib/users_grid.dart
+++ lib/users_grid.dart
@@ -1,41 +1,5 @@
-import 'package:flutter/material.dart';
-
-import 'user_card.dart';
-
-class UsersGrid extends StatelessWidget {
-  final List<dynamic> users;
-  final Set<int> selectedIds;
-  final Function(int) onSelect;
-  final Function(dynamic) onEdit;
-
-  const UsersGrid({
-    super.key,
-    required this.users,
-    required this.selectedIds,
-    required this.onSelect,
-    required this.onEdit,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    final screenWidth = MediaQuery.of(context).size.width;
-    final crossAxisCount = screenWidth > 900 ? 3 : (screenWidth > 600 ? 2 : 1);
-
-    return GridView.builder(
-      shrinkWrap: true,
-      physics: const NeverScrollableScrollPhysics(),
-      padding: const EdgeInsets.all(24),
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: crossAxisCount,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
-        childAspectRatio: 1.6,
-      ),
-      itemCount: users.length,
-      itemBuilder: (context, index) {
-        final user = users[index];
         return UserCard(
-          name: user['name'],
+          name: user['name'] ?? 'Unknown',
           location: user['location'] ?? '',
           avatarUrl: user['avatarUrl'] ?? '',
           tags: List<String>.from(user['tags'] ?? []),
@@ -43,7 +7,3 @@ class UsersGrid extends StatelessWidget {
           onTap: () => onSelect(user['id']),
           onLongPress: () => onEdit(user),
         );
-      },
-    );
-  }
-}
