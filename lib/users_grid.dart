42625cef24ea0762120517a4edf05baa923c4701
Liorbo <master010590@gmail.com>
2026-01-11 17:43:28 +0200

    Create UsersGrid widget for displaying users in a responsive grid layout

---
+++ lib/users_grid.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+import 'user_card.dart';
+
+class UsersGrid extends StatelessWidget {
+  final List<dynamic> users;
+  final Set<int> selectedIds;
+  final Function(int) onSelect;
+
+  const UsersGrid({
+    super.key,
+    required this.users,
+    required this.selectedIds,
+    required this.onSelect,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final crossAxisCount = screenWidth > 900 ? 3 : (screenWidth > 600 ? 2 : 1);
+
+    return GridView.builder(
+      padding: const EdgeInsets.all(24),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: crossAxisCount,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
+        childAspectRatio: 1.4,
+      ),
+      itemCount: users.length,
+      itemBuilder: (context, index) {
+        final user = users[index];
+        return UserCard(
+          name: user['name'],
+          location: user['location'],
+          avatarUrl: user['avatarUrl'],
+          tags: List<String>.from(user['tags']),
+          isSelected: selectedIds.contains(user['id']),
+          onTap: () => onSelect(user['id']),
+        );
+      },
+    );
+  }
+}
