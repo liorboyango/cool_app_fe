b7a82aa3137d0a34450d5ad1ae66f2bbb7744a96
Liorbo <master010590@gmail.com>
2026-01-11 17:56:18 +0200

    Update UsersGrid to match UI/UX design: use User model, selectedIndex, remove onEdit

--- lib/users_grid.dart
+++ lib/users_grid.dart
@@ -1,9 +1,47 @@
+import 'package:flutter/material.dart';
+
+import 'user.dart';
+import 'user_card.dart';
+
+class UsersGrid extends StatelessWidget {
+  final List<User> users;
+  final int? selectedIndex;
+  final Function(int) onSelect;
+
+  const UsersGrid({
+    super.key,
+    required this.users,
+    this.selectedIndex,
+    required this.onSelect,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final crossAxisCount = screenWidth > 900 ? 3 : (screenWidth > 600 ? 2 : 1);
+
+    return GridView.builder(
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      padding: const EdgeInsets.all(24),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: crossAxisCount,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
+        childAspectRatio: 1.6,
+      ),
+      itemCount: users.length,
+      itemBuilder: (context, index) {
+        final user = users[index];
         return UserCard(
-          name: user['name'] ?? 'Unknown',
-          location: user['location'] ?? '',
-          avatarUrl: user['avatarUrl'] ?? '',
-          tags: List<String>.from(user['tags'] ?? []),
-          isSelected: selectedIds.contains(user['id']),
-          onTap: () => onSelect(user['id']),
-          onLongPress: () => onEdit(user),
+          name: user.name,
+          location: user.location,
+          avatarUrl: user.avatarUrl,
+          tags: user.tags,
+          isSelected: selectedIndex == index,
+          onTap: () => onSelect(index),
         );
+      },
+    );
+  }
+}
