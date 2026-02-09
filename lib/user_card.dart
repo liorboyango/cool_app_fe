651006e89a547e66e72bd40d997029fe54e4c7cb
Liorbo <master010590@gmail.com>
2026-02-09 18:45:36 +0200

    Add linkedinUrl parameter and LinkedIn IconButton to UserCard

--- lib/user_card.dart
+++ lib/user_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class UserCard extends StatelessWidget {
@@ -11,6 +12,7 @@ class UserCard extends StatelessWidget {
   final String email;
   final String? phoneNumber;
   final String? facebookUrl;
+  final String? linkedinUrl;
   final bool isSelected;
   final VoidCallback? onTap;
   final VoidCallback? onEdit;
@@ -27,6 +29,7 @@ class UserCard extends StatelessWidget {
     required this.email,
     this.phoneNumber,
     this.facebookUrl,
+    this.linkedinUrl,
     this.isSelected = false,
     this.onTap,
     this.onEdit,
@@ -149,6 +152,18 @@ class UserCard extends StatelessWidget {
                       ),
                     ),
                   ),
+                  if (linkedinUrl != null && linkedinUrl!.isNotEmpty) ...[
+                    const SizedBox(height: 8),
+                    IconButton(
+                      icon: const FaIcon(
+                        FontAwesomeIcons.linkedin,
+                        color: Color(0xFF0A66C2),
+                        size: 20,
+                      ),
+                      onPressed: () => _launchUrl(linkedinUrl!),
+                      tooltip: 'LinkedIn Profile',
+                    ),
+                  ],
                   if (onEdit != null) ...[
                     const SizedBox(height: 8),
                     IconButton(
